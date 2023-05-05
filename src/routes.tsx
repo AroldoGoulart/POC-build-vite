eeb11bab1cb9266e429d1fa27408ecf2aba5e46d
Aroldo Goulart <aroldogooulart@gmail.com>
2023-05-05 17:10:56 -0400

    fix: retirar rota funcional

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,12 +7,6 @@ import Rota1 from "./screens/Rota1";
 import Rota2 from "./screens/Rota2";
 import Rota3 from "./screens/Rota3";
 
-/*
-  O componente da rota será carregado somente quando 
-  o usuário acessar a rota específica. 
-*/
-const DynamicRota = lazy(() => import("./screens/RotaDinamica"));
-
 function App() {
   return (
     <BrowserRouter>
@@ -21,11 +15,6 @@ function App() {
         <Route path="/rota1" element={<Rota1 />} />
         <Route path="/rota2" element={<Rota2 />} />
         <Route path="/rota3" element={<Rota3 />} />
-        <Route path="/dynamic">
-          <Suspense fallback={<div>Carregando rota dinâmica...</div>}>
-            <DynamicRota />
-          </Suspense>
-        </Route>
       </Routes>
       <Outlet />
     </BrowserRouter>
